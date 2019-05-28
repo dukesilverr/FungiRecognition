57ad2e8d88a42e3960104470e9f7396c832f9c84
DovilePatiejunaite <dpatiejunaite@gmail.com>
2019-05-29 02:59:00 +0300

    Working version with 2 languages

--- app.py
+++ app.py
@@ -3,9 +3,8 @@
 # imports
 import tensorflow as tf
 import numpy as np
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, abort
 from werkzeug import secure_filename
-import time
 import base64
 import io
 from keras.applications.resnet50 import preprocess_input
@@ -15,25 +14,35 @@ from PIL import Image
 from urllib.parse import quote
 import uuid
 import configparser
+
+
+# reading labels for bilingual app and fungi descriptions
 config = configparser.RawConfigParser()
 config.read('label.properties')
+
+
 # loading model
 model = load_model('classifier1.h5')
 model._make_predict_function()
 configTf = tf.ConfigProto()
 configTf.gpu_options.allow_growth = True
 sess = tf.Session(config=configTf)
+app = Flask(__name__)
+app.secret_key = 'bioinformatics'
+
 # app constants
-ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'png'])
+ALLOWED_EXTENSIONS = ['jpg', 'jpeg', 'png']
+ALLOWED_LANGUAGES = ['en', 'lt']
 IMAGE_RESIZE = 256
 IMAGE_LIMIT = 300
+LABELS = sorted(["Agaricus","Amanita","Boletus","Cantharellus","Cortinarius", "Hygrocybe", "Lactarius",
+                 "Not fungi", "Russula", "Suillus"])
 
 
 class Prediction:
-    def __init__(self, source, label, image_name, score, label_lt, about):
+    def __init__(self, source, label, image_name, score, about):
         self.source = source
         self.label = label
-        self.label_lt = label_lt
         self.image_name = image_name
         self.score = score
         self.id = str(uuid.uuid4())
@@ -45,15 +54,15 @@ def predict(file):
     img = Image.open(buf)
     img = img.resize((IMAGE_RESIZE, IMAGE_RESIZE))
     x = image.img_to_array(img)
+
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
     gg = model.predict(x)
     print(gg.argmax(axis=-1))
     classes = gg.argmax(axis=-1)[0]
-    procent = round(gg[0, classes] * 100, 2)
-    labels = ["Agaricus","Amanita","Boletus","Cantharellus","Cortinarius", "Hygrocybe", "Lactarius", "Not fungi", "Russula", "Suillus"]
-    predicted_label = sorted(labels)[classes]
-    return predicted_label, procent
+    percent = round(gg[0, classes] * 100, 2)
+    predicted_label = LABELS[classes]
+    return predicted_label, percent
 
 
 def allowed_file(filename):
@@ -61,41 +70,47 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
-app = Flask(__name__)
-app.secret_key = 'bioinformatics'
-
-
 @app.route("/", methods=['GET'])
-def reload():
-    return render_template('index.html')
+def redirec_to_en():
+    return redirect('/en')
 
 
-@app.route('/', methods=['POST'])
-def submit_file_for_prediction():
+@app.route("/<lang>", methods=['GET'])
+def reload(lang):
+    if lang not in ALLOWED_LANGUAGES:
+        abort(404)
+    return render_template('index.html', copyrights=config.get(lang, 'copyrights'), h1=config.get(lang, 'h1'),
+                           h2=config.get(lang, 'h2'), choose_file=config.get(lang, 'choose_file'),
+                           submit=config.get(lang, 'submit'), render_results=False)
+
+
+@app.route('/<lang>', methods=['POST', 'GET'])
+def submit_file_for_prediction(lang):
     if request.method == 'POST':
-        import time
-        start_time = time.time()
         predictions = []
-        print(len(request.files.getlist('images')))
         if len(request.files.getlist('images')) > 30:
-            error = 'Only 30 images are allowed for prediction.'
-            flash(error)
-            return redirect(url_for('submit_file_for_prediction'))
+            flash(config.get(lang, 'error_30'))
+            return redirect(url_for('submit_file_for_prediction'), lang=lang)
         for file in request.files.getlist('images'):
             file_content = file.read()
             file_b64 = base64.b64encode(file_content).decode('ascii')
             if file and allowed_file(file.filename):
                 filename = secure_filename(file.filename)
-                label, procent = predict(file_content)
-                print("--- %s seconds ---" % str(time.time() - start_time))
+                label, percent = predict(file_content)
                 data_url = 'data:image/png;base64,{}'.format(quote(file_b64))
-                prediction = Prediction(data_url, label, filename, procent, config.get(label, 'name_lt'), config.get(label, 'about'))
+                prediction = Prediction(data_url, config.get(label, 'name_%s' % lang), filename, percent,
+                                        config.get(label, 'about_%s' % lang))
                 predictions.append(prediction)
             else:
-                error = 'Please select file with allowed file format: jpg/jpeg/png.'
-                flash(error)
-                return redirect(url_for('submit_file_for_prediction'))
-        return render_template('template.html', predictions=predictions)
+                flash(config.get(lang, 'error_file'))
+                return redirect(url_for('submit_file_for_prediction', lang=lang))
+        return render_template('index.html', predictions=predictions, copyrights=config.get(lang, 'copyrights'),
+                               h1=config.get(lang, 'h1'), h2=config.get(lang, 'h2'),
+                               choose_file=config.get(lang, 'choose_file'), submit=config.get(lang, 'submit'),
+                               prediction=config.get(lang, 'prediction'), score=config.get(lang, 'score'),
+                               results=config.get(lang, 'results'), image=config.get(lang, 'image'),
+                               read_more=config.get(lang, 'read_more'), render_results=True)
+
 
 if __name__ == "__main__":
     app.debug=False
--- static/main.js
+++ static/main.js
@@ -5,15 +5,20 @@ $('document').ready(function(){
         input.addEventListener( 'change', function( e )
             {
                 var fileName = '';
-                if( this.files && this.files.length > 1 )
-                    fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
-                else
+                if( this.files && this.files.length > 1 ) {
+                    if(window.location.pathname.includes("lt")) {
+                        fileName= this.files.length + " pasirinkti failai"
+                    } else {
+                    	fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+		 	}
+		} else {
                     fileName = e.target.value.split( '\\' ).pop();
-
-                if( fileName )
+		}
+                if( fileName ) {
                     label.querySelector( 'span' ).innerHTML = fileName;
-                else
+                } else {
                     label.innerHTML = labelVal;
+		}
         });
 });
 
